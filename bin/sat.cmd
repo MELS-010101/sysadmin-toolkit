bf1d1be585a500e9c190d53da19cf208dda09530
MELS-010101 <subrosa1996@gmail.com>
2026-05-22 12:29:52 +0300

    fix: clean up sat.cmd (v1.3.0)

--- bin/sat.cmd
+++ bin/sat.cmd
@@ -1,24 +1,2 @@
-@'
 @echo off
 "C:\Program Files\Git\bin\bash.exe" "%~dp0sat" %*
-
-REM Get script directory
-set "SCRIPT_DIR=%~dp0"
-set "ROOT_DIR=%SCRIPT_DIR:~0,-1%"
-
-REM Check if WSL is available
-where wsl >nul 2>nul
-if %errorlevel% equ 0 (
-    wsl bash "%ROOT_DIR%/bin/sat" %*
-    exit /b %errorlevel%
-)
-
-REM Check if Git Bash is available
-if exist "C:\Program Files\Git\bin\bash.exe" (
-    "C:\Program Files\Git\bin\bash.exe" "%ROOT_DIR%/bin/sat" %*
-    exit /b %errorlevel%
-)
-
-echo Error: Neither WSL nor Git Bash found. Please install one of them.
-echo For Windows subsystem, install WSL: https://aka.ms/wsl
-echo Or install Git for Windows: https://git-scm.com/download/win
